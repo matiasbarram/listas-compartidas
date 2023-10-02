860ec400800fd605ba38d55cbce8de928e46c12d
Matias Barra <matias.barra@alumnos.uach.cl>
2023-10-02 13:56:31 -0300

    Add zod and login validations

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,25 +1,72 @@
 "use client"
 
-import React, { useState } from "react"
-import { useRouter } from "next/navigation"
-import { signIn } from "next-auth/react"
-import Link from "next/link"
-export default function LoginPage() {
-    const router = useRouter()
-    const [data, setData] = useState({
-        email: "",
-        password: ""
-    })
+import React, { useEffect, useState } from "react";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { createToast } from "@/app/lib/common";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ISignUpFormValues, signUpSchema } from "../../../../types";
+import { z } from "zod";
+import Spinner from "@/app/components/common/Spinner";
 
-    const loginUser = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        signIn("credentials", {
-            email: data.email,
-            password: data.password,
-            redirect: true,
-            callbackUrl: "/home"
+const ForgotPassword = () => {
+    return (
+        <div className="flex items-center justify-between">
+            <Link href="#" className="text-sm font-medium hover:underline text-primary-500">
+                ¿Olvidaste tu contraseña?
+            </Link>
+        </div>
+    )
+}
+
+
+const checkErrors = (router: AppRouterInstance): void => {
+    const params = new URLSearchParams(window.location.search);
+    const callbackUrl = params.get("callbackUrl");
+    const error = params.get("error");
+    if (error) {
+        createToast({
+            toastType: "error",
+            message: "Error al iniciar sesión, por favor intenta de nuevo."
         })
     }
+}
+
+export default function LoginPage() {
+    const router = useRouter();
+    checkErrors(router);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<ISignUpFormValues>({
+        resolver: zodResolver(signUpSchema),
+    });
+
+
+
+    const loginUser = async (data: ISignUpFormValues) => {
+        try {
+            const validatedData = signUpSchema.parse(data);
+            await signIn("credentials", {
+                email: validatedData.email,
+                password: validatedData.password,
+                redirect: true,
+                callbackUrl: "/home",
+            });
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                const { fieldErrors } = error.flatten();
+            }
+        }
+        finally {
+        }
+    };
+
     return (
         <section className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div className="w-full  rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 border-zinc-700 bg-zinc-800">
@@ -27,41 +74,63 @@ export default function LoginPage() {
                     <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                         Ingresa con tu cuenta
                     </h1>
-                    <form className="space-y-4 md:space-y-6" onSubmit={loginUser}>
+                    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(loginUser)}>
                         <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Correo</label>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">
+                                Correo
+                            </label>
                             <input
                                 type="email"
+                                {
+                                ...register("email")
+                                }
                                 name="email"
                                 id="email"
-                                className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                                className={`border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500`}
                                 placeholder="name@company.com"
-                                required={true}
-                                onChange={(e) => setData({ ...data, email: e.target.value })}
                             />
+                            {errors.email ? <p className="text-xs text-red-500">{errors.email.message}</p> : null}
                         </div>
                         <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-mediumtext-white">Contraseña</label>
+                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">
+                                Contraseña
+                            </label>
                             <input
                                 type="password"
+                                {
+                                ...register("password")
+                                }
                                 name="password"
                                 id="password"
                                 placeholder="••••••••"
-                                className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                                required={true}
-                                onChange={(e) => setData({ ...data, password: e.target.value })}
+                                className={`border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500`}
                             />
+                            {errors.password ? <p className="text-xs text-red-500">{errors.password.message}</p> : null}
+
+
                         </div>
-                        <div className="flex items-center justify-between">
-                            <Link href="#" className="text-sm font-medium hover:underline text-primary-500">¿Olvidaste tu contraseña?</Link>
-                        </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-slate-600 hover:bg-slate-700 focus:ring-slate-800">Sign in</button>
-                        <p className="text-sm font-light text-gray-400">
-                            ¿Aún no tienes cuenta? <Link href="/auth/register" className="font-medium hover:underline text-primary-500">Regístrate</Link>
-                        </p>
+                        <ForgotPassword />
+                        <button
+                            type="submit"
+                            disabled={isSubmitting}
+                            className={`w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-slate-600 hover:bg-slate-700 focus:ring-slate-800 focus:border-slate-800`}
+                        >
+                            {
+                                !isSubmitting ? "Iniciar sesión" : (
+                                    <Spinner />
+                                )
+                            }
+                        </button>
                     </form>
+
+                    <p className="text-sm font-light text-gray-400">
+                        ¿Aún no tienes cuenta?{" "}
+                        <Link href="/auth/register" className="font-medium hover:underline text-primary-500">
+                            Regístrate
+                        </Link>
+                    </p>
                 </div>
             </div>
         </section>
-    )
+    );
 }
