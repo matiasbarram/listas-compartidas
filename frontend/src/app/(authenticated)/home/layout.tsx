4db62565f2041cc27265845bb6607ca3a9a8e85c
Matias Barra <matias.barra@alumnos.uach.cl>
2023-10-13 00:19:11 -0300

    fix logout when server jwt expire

--- frontend/src/app/(authenticated)/home/layout.tsx
+++ frontend/src/app/(authenticated)/home/layout.tsx
@@ -1,9 +1,32 @@
+"use client"
+
 import { Header } from "@/app/components/Header/Header"
-import React from "react"
+import { API_URL } from "@/lib/constants"
+import { signOut, useSession } from "next-auth/react"
+import React, { useEffect } from "react"
 
 export default function DashboardLayout({ children }: {
     children: React.ReactNode
 }) {
+    const { data: session, status } = useSession()
+    useEffect(() => {
+        const checkJwt = async () => {
+            fetch(API_URL + "   ", {
+                headers: {
+                    Authorization: `Bearer ${session?.token}`,
+                },
+            }).then((res) => {
+                if (res.status === 401) {
+                    signOut()
+                }
+            })
+        }
+        if (status === "authenticated") {
+            checkJwt()
+        }
+
+    }, [session, status])
+
     return (
         <main>
             <Header />
