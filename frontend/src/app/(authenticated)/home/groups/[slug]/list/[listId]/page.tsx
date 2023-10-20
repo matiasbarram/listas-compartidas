f3640a49311bc232602567f9850643a7c87f175a
Matias Barra <matias.barra@alumnos.uach.cl>
2023-10-20 20:46:52 -0300

    fix removed import

--- frontend/src/app/(authenticated)/home/groups/[slug]/list/[listId]/page.tsx
+++ frontend/src/app/(authenticated)/home/groups/[slug]/list/[listId]/page.tsx
@@ -5,8 +5,6 @@ import { BackBtn } from "@/app/components/common/BackBtn";
 import { ItemsGrid } from "@/app/components/home/lists/grid/itemsGrid";
 import { getListItems } from "@/lib/actions/item/items";
 import { NestedParams } from "../../../../../../../../types";
-import ItemsProvider from "@/providers/ItemsProvider";
-
 
 export default async function ListItemPage({ params }: NestedParams) {
     const session = await getServerSession(authOptions);
