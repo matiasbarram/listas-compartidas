b18e462369d587aaca12127ea6e4213c3ae0ff85
Matías Barra <matias.barra@alumnos.uach.cl>
2026-06-21 01:56:54 -0400

    fix: suppress eslint warning for spread in useEffect deps

--- frontend/src/hooks/clickOutside.ts
+++ frontend/src/hooks/clickOutside.ts
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 
-export function useOutsideClick(ref: any, onClickOut: () => void, deps = []) {
+export function useOutsideClick(ref: any, onClickOut: () => void, deps: unknown[] = []) {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
         const onClick = ({ target }: any) =>
             !ref?.current?.contains(target) && onClickOut?.()
